a02c6ae0f1f89b30cc368130873c9f9194975c1a
Rodion Abdurakhimov <rodion.arr.web@gmail.com>
2021-11-12 18:13:41 +0200

    Add application configs

---
+++ api/config/configuration.ts
@@ -0,0 +1,23 @@
+export const getConfig = (): AppConfig => ({
+  port: parseInt(process.env.PORT, 10) || 3000,
+  database: {
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT, 10) || 5432,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    dbName: process.env.DB_NAME,
+  },
+});
+
+export interface AppConfig {
+  port: number;
+  database: DbConfig;
+}
+
+export interface DbConfig {
+  host: string;
+  port: number;
+  user: string;
+  password: string;
+  dbName: string;
+}
--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { getConfig } from '../config/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      load: [getConfig],
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
