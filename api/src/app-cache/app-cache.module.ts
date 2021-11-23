c115321297bedd0792646dccd27f32fce0969036
Rodion Abdurakhimov <rodion.arr.web@gmail.com>
2021-11-23 15:45:02 +0200

    feat: Add graceful shutdown

--- api/src/app-cache/app-cache.module.ts
+++ api/src/app-cache/app-cache.module.ts
@@ -1,9 +1,19 @@
-import { CacheModule, Module } from '@nestjs/common';
+import {
+  CACHE_MANAGER,
+  CacheModule,
+  Inject,
+  Logger,
+  Module,
+  OnModuleDestroy,
+} from '@nestjs/common';
 import { CacheConfigService } from './services/cache-config/cache-config.service';
 import { ConfigModule } from '@nestjs/config';
+import { Cache } from 'cache-manager';
+import { LoggerModule } from '../logger/logger.module';
 
 @Module({
   imports: [
+    LoggerModule,
     ConfigModule,
     CacheModule.registerAsync({
       imports: [ConfigModule],
@@ -13,4 +23,13 @@ import { ConfigModule } from '@nestjs/config';
   providers: [CacheConfigService],
   exports: [CacheModule],
 })
-export class AppCacheModule {}
+export class AppCacheModule implements OnModuleDestroy {
+  private readonly logger = new Logger(AppCacheModule.name);
+
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+
+  onModuleDestroy(): any {
+    this.logger.log('Disconnecting from cache');
+    (this.cacheManager.store as any).getClient().end(true);
+  }
+}
--- api/src/main.ts
+++ api/src/main.ts
@@ -13,6 +13,8 @@ async function bootstrap() {
     new FastifyAdapter(),
   );
 
+  app.enableShutdownHooks();
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
