15a5597dc90bed33a279a6a33e9d248cdc4d8205
Rodion Abdurakhimov <rodion.arr.web@gmail.com>
2024-01-02 21:01:18 +0200

    Drop uuid package

--- api/src/global/middleware/async-storage/async-storage.middleware.ts
+++ api/src/global/middleware/async-storage/async-storage.middleware.ts
@@ -1,5 +1,5 @@
 import { Inject, Injectable, NestMiddleware } from '@nestjs/common';
-import { v4 as uuid } from 'uuid';
+import { randomUUID } from 'node:crypto';
 import { ASYNC_STORAGE } from '../../constants';
 import { AsyncLocalStorage } from 'async_hooks';
 
@@ -11,7 +11,7 @@ export class AsyncStorageMiddleware implements NestMiddleware {
   ) {}
 
   use(req: any, res: any, next: () => void) {
-    const traceId = req.headers['x-request-id'] || uuid();
+    const traceId = req.headers['x-request-id'] || randomUUID();
     const store = new Map().set('traceId', traceId);
     this.asyncStorage.run(store, () => {
       next();
