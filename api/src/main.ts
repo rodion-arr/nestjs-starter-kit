a988d6a913f77e8d68475d86d70a92e39348b5a1
Rodion Abdurakhimov <rodion.arr.web@gmail.com>
2024-02-06 00:08:07 +0200

    feat: Add security header via helmet package

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppLoggerService } from './logger/services/app-logger/app-logger.service';
 import { json } from 'body-parser';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,8 @@ async function bootstrap() {
     and not loose context on POST requests
    */
   app.use(json());
+
+  app.use(helmet());
 
   app.enableShutdownHooks();
 
