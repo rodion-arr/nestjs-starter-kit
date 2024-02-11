fe21e0d6066a62d4aa73686db99afe9b7e45c128
Rodion Abdurakhimov <rodion.arr.web@gmail.com>
2024-02-11 15:12:23 +0200

    fix: test

--- api/src/health/health.controller.spec.ts
+++ api/src/health/health.controller.spec.ts
@@ -3,10 +3,12 @@ import { HealthController } from './health.controller';
 import { HealthCheckService, TypeOrmHealthIndicator } from '@nestjs/terminus';
 import { HealthCheckResult } from '@nestjs/terminus/dist/health-check/health-check-result.interface';
 import Mock = jest.Mock;
+import { CacheHealthIndicator } from './indicators/cache/cache.health-indicator';
 
 describe('HealthController', () => {
   let healthController: HealthController;
   let healthCheckService: Partial<HealthCheckService>;
+  let cacheHealthIndicator: Partial<CacheHealthIndicator>;
   let typeOrmHealthIndicator: Partial<TypeOrmHealthIndicator>;
 
   beforeEach(async () => {
@@ -18,11 +20,16 @@ describe('HealthController', () => {
       pingCheck: jest.fn(),
     };
 
+    cacheHealthIndicator = {
+      isHealthy: jest.fn(),
+    };
+
     const moduleRef = await Test.createTestingModule({
       controllers: [HealthController],
       providers: [
         { provide: HealthCheckService, useValue: healthCheckService },
         { provide: TypeOrmHealthIndicator, useValue: typeOrmHealthIndicator },
+        { provide: CacheHealthIndicator, useValue: cacheHealthIndicator },
       ],
     }).compile();
 
@@ -35,10 +42,18 @@ describe('HealthController', () => {
 
   describe('healthCheck', () => {
     it('should return health check result', async () => {
+      (cacheHealthIndicator.isHealthy as Mock).mockResolvedValue({
+        cache: {
+          status: 'up',
+        },
+      });
       const healthCheckResult: HealthCheckResult = {
         status: 'ok',
         details: {
           db: {
+            status: 'up',
+          },
+          cache: {
             status: 'up',
           },
         },
@@ -55,6 +70,7 @@ describe('HealthController', () => {
       const result = await healthController.healthCheck();
 
       expect(healthCheckService.check).toHaveBeenCalledWith([
+        expect.any(Function),
         expect.any(Function),
       ]);
       expect(typeOrmHealthIndicator.pingCheck).toHaveBeenCalledWith('db');
